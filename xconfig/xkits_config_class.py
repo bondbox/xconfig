07f9b394bee7151f0a79602e097f9fe97bcf27d0
Mingzhe Zou <zoumingzhe@qq.com>
2026-05-31 10:37:30 +0800

    xconfig-attrs: update type hints and return types

--- xconfig/xkits_config_class.py
+++ xconfig/xkits_config_class.py
@@ -1,7 +1,8 @@
 # coding:utf-8
 
 from typing import Any
-from typing import Generator
+from typing import Iterator
+from typing import List
 from typing import Type
 
 from xkits_config_annot import Annot
@@ -9,11 +10,11 @@ from xkits_config_annot import Annot
 try:
     from xkits_config_attrs import parse_attrs
 except ImportError:  # pragma: no cover
-    def parse_attrs(cls: Type[Any]) -> Generator[Annot, Any, None]:  # noqa:E501, pragma: no cover
+    def parse_attrs(cls: Type[Any]) -> Iterator[Annot]:  # pragma: no cover
         raise NotImplementedError("Module xkits-config-attrs is not installed")
 
 
-def parse_dataclass(cls: Type[Any]) -> Generator[Annot, Any, None]:
+def parse_dataclass(cls: Type[Any]) -> Iterator[Annot]:
     from dataclasses import MISSING  # pylint: disable=C0415
     from dataclasses import fields  # pylint: disable=C0415
     for field in fields(cls):  # type: ignore
@@ -22,7 +23,7 @@ def parse_dataclass(cls: Type[Any]) -> Generator[Annot, Any, None]:
         yield Annot(field.name, field.type, default)
 
 
-def parse(cls: Type[Any]):
+def parse(cls: Type[Any]) -> List[Annot]:
     try:
         return list(parse_dataclass(cls))
     except TypeError:
--- xconfig_attr/xkits_config_attrs.py
+++ xconfig_attr/xkits_config_attrs.py
@@ -1,13 +1,13 @@
 # coding:utf-8
 
 from typing import Any
-from typing import Generator
+from typing import Iterator
 from typing import Type
 
 from xkits_config_annot import Annot
 
 
-def parse_attrs(cls: Type[Any]) -> Generator[Annot, Any, None]:
+def parse_attrs(cls: Type[Any]) -> Iterator[Annot]:
     from attr import Attribute  # pylint: disable=C0415
     from attr import NOTHING  # pylint: disable=C0415
     from attr import fields  # pylint: disable=C0415
