91c053d7f728ba912c5cdff9bdfa6c98c2486496
Mingzhe Zou <zoumingzhe@qq.com>
2026-06-15 02:23:00 +0800

    build: remove unused xpip-build dependency from pyproject.toml

--- xconfig/attribute.py
+++ xconfig/attribute.py
@@ -1,8 +1,11 @@
 # coding:utf-8
 
-__version__ = "0.7"
-
 # project info
 __project_name__ = "xconfig"
 __project_home__ = "https://github.com/bondbox/xconfig/"
 __project_desc__ = "Config module"
+
+# package info
+__version__ = "0.7"
+__package_name__ = "xkits-config"
+__package_desc__ = "Config module"
